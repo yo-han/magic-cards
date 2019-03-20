b3ec667f80c480ec184c7cb6848146b62ae513be
Yo-han <github@johankuijt.com>
2019-03-20 19:54:13 +0000

    NFC acr122u reader support

--- scanner/scanner.js
+++ scanner/scanner.js
@@ -1,24 +1,54 @@
-const InputEvent = require('input-event')
 const cardProcessor = require('./CardProcessor')
 const config = require(__dirname + '/../config/config.json')
 
-const keys = 'X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX'
-const input = new InputEvent(`/dev/input/${config.input_device}`)
-const keyboard = new InputEvent.Keyboard(input)
-
 let string = ''
 
-keyboard.on('keyup', function(event) {
-  // Enter key
-  if (event.code == 28) {
-    console.log(`Read Card ID: ${string}`)
+if(config.input_device == 'acr122') {
 
-    //process code
-    cardProcessor.process(string)
+  const { NFC } = require("nfc-pcsc");
+  const nfc = new NFC();
 
-    // reset string for next scan
-    string = ''
-  } else {
-    string += keys[event.code]
-  }
-})
+  nfc.on('reader', reader => {
+
+    console.log(reader.name + ' reader attached, waiting for cards ...');
+
+    reader.on('card', card => {
+	string = card.uid;
+
+    });
+
+    reader.on('error', err => {
+	console.error('reader error', err);
+    });
+
+    reader.on('end', () => {
+	console.log(reader.name + ' reader disconnected.');
+    });
+  });
+
+  nfc.on('error', err => {
+    console.error(err);
+  });
+
+} else {
+
+  const InputEvent = require('input-event')
+  const keys = 'X^1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX'
+  const input = new InputEvent(`/dev/input/${config.input_device}`)
+  const keyboard = new InputEvent.Keyboard(input)
+
+  keyboard.on('keyup', function(event) {
+    // Enter key
+    if (event.code == 28) {
+      console.log(`Read Card ID: ${string}`)
+
+      //process code
+      cardProcessor.process(string)
+
+      // reset string for next scan
+      string = ''
+    } else {
+      string += keys[event.code]
+    }
+  });
+}
